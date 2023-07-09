a5b79b82770b08954df306d641026079714516c1
Faizik12 <zerg9119@gmail.com>
2023-07-09 11:33:54 +0500

    completed the second step

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,20 @@
 import pytest
+from page_analyzer import app
 
 
-def test_pass():
-    pass
+@pytest.fixture()
+def app_testing():
+    app.config.update({
+        "TESTING": True,
+    })
+    yield app
+
+
+@pytest.fixture
+def client(app_testing):
+    return app_testing.test_client()
+
+
+def test_render(client):
+    response = client.get('/')
+    assert b'<h2>Hello, World!</h2>' in response.data
